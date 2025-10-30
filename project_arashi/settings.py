d37e0052cf77dbaa0fbc3c57da9b614e2268cc81
kznoost <MtE1523h@gmail.com>
2025-10-31 01:46:59 +0900

    Initial commit

--- project_arashi/settings.py
+++ project_arashi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k%gzt1yf#x_7k4nd75j2$lu-03k=p9o32v6gdo$-z!d&rmhg^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    'your-app.onrender.com', '127.0.0.1'
+    'arashi_yjjkg.onrender.com', '127.0.0.1'
 ]
 
 
